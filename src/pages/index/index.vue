40cb7abaa467ede423162bbe714e48c578449187
koolss <koolss@koolss.com>
2021-09-07 17:28:02 +0800

    初始化项目

---
+++ src/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <view>
+    <view class='at-row'>
+      <view class='at-col'>A</view>
+      <view class='at-col'>B</view>
+      <view class='at-col'>C</view>
+    </view>
+  </view>
+</template>
+
+<script>
+import './index.scss'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
